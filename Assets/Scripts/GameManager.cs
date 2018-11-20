c0bf10df8a74d337936fe16e7773d9845f1612d0
Kyohei <kalops22.kikoo@gmail.com>
2018-11-20 14:17:07 +0100

    Ajout de 4 Niveaux
    Ajout des 4 premiers niveaux
    Ajout du system de transition de niveaux
    Ajout de la gestion de fin de niveau

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    int currentLevel = 1;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void loadNextLevel()
+    {
+        int temp = currentLevel + 1;
+        SceneManager.LoadScene("Level" + temp);
+        currentLevel++;
+    }
+}
