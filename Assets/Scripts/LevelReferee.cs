0a3122eee6178fabba9288e29100b02b319cfca8
Kentarosan <jacquin.yannis@gmail.com>
2018-11-28 16:09:38 +0100

    Ajout UI InGame + création LevelReferee

---
+++ Assets/Scripts/LevelReferee.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelReferee : MonoBehaviour {
+
+    public List<ReceptacleCube> receptaclesCube = new List<ReceptacleCube>();
+    public Couloir entryCorridor;
+    public Couloir exitCorridor;
+
+    private bool isLevelFinished = false;
+
+	// Use this for initialization
+	void Start () {
+		if (receptaclesCube.Count <= 0 || entryCorridor == null || exitCorridor == null)
+        {
+            Debug.Break();
+            Debug.LogError("Le level Referee n'a pas toutes les infos requises.");
+        }
+        foreach (ReceptacleCube receptacle in receptaclesCube)
+        {
+            receptacle.activateEvent.AddListener(UpdateWinConditions);
+            Debug.Log(receptacle.name + " inscrittion");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void UpdateWinConditions()
+    {
+        isLevelFinished = true;
+        foreach(ReceptacleCube receptacle in receptaclesCube)
+        {
+            if (!receptacle.activated)
+                isLevelFinished = false;
+        }
+
+        if (isLevelFinished)
+            Debug.Log("Niveau fini!");
+    }
+}
