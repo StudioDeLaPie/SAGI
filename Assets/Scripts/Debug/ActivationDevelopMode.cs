29084a6f6423be3832fd406dd7effb7037e6570e
Kyohei <kalops22.kikoo@gmail.com>
2018-12-10 10:42:59 +0100

    Ajout du mode developpement

---
+++ Assets/Scripts/Debug/ActivationDevelopMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationDevelopMode : MonoBehaviour
+{
+    [SerializeField] List<GameObject> Debugs;
+    bool active;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {            
+            if (Input.GetKey(KeyCode.KeypadEnter))
+            {
+                active = true;
+                Debug.Log("Enter");
+            }
+            else
+                active = false;
+
+            for (int i = 0; i < Debugs.Count; i++)
+            {
+                Debugs[i].SetActive(active);
+            }
+        }
+    }
+}
