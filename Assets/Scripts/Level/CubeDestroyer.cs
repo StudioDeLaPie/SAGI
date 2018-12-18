5fb4b90391741ade5432a7033d95edc36642adcb
Kentarosan <jacquin.yannis@gmail.com>
2018-12-18 16:57:34 +0100

    Creation Lvl multi 3 + creation d'assets de niveau
    Assets CubeDestroyer et ObjectActivator

---
+++ Assets/Scripts/Level/CubeDestroyer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CubeDestroyer : NetworkBehaviour {
+
+    public GameObject cube;
+
+    private Vector3 cubeStartPos;
+
+    private void Awake()
+    {
+        cubeStartPos = cube.transform.root.position;
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (!isServer)
+            Destroy(this);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.root.gameObject == cube)
+        {
+            StartCoroutine(DestroyCoroutine());
+        }
+    }
+
+    private IEnumerator DestroyCoroutine()
+    {
+        float startTime = Time.time;
+        cube.transform.position = new Vector3(0, -15, 0);
+        cube.GetComponent<Weight>().CmdStop();
+        while (Time.time < startTime + 3)
+        {
+            yield return null;
+        }
+        cube.GetComponent<Weight>().CmdIncreaseWeight();
+        cube.transform.position = cubeStartPos;
+    }
+}
