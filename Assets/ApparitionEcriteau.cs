6df5781959989cbc9ed00c5b7f98e6a500db46f2
Kentarosan <jacquin.yannis@gmail.com>
2018-12-18 11:42:00 +0100

    Finition des 5 Levels tuto

---
+++ Assets/ApparitionEcriteau.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApparitionEcriteau : MonoBehaviour {
+
+    private Vector3 startScale;
+
+	// Use this for initialization
+	void Start () {
+        startScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+        FindObjectOfType<ReceptacleCube>().activationEvent.AddListener(Apparition);
+	}
+	
+	private void Apparition()
+    {
+        StartCoroutine(ApparitionCoroutine());
+    }
+
+    private IEnumerator ApparitionCoroutine()
+    {
+        Vector3 newScale = startScale / 10;
+        while (newScale.x < startScale.x)
+        {
+            newScale *= 1.1f;
+            transform.localScale = newScale;
+            yield return null;
+        }
+    }
+}
