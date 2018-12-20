dc706f616cc66743e3330474a7fbe4d2447e6d1a
Kentarosan <jacquin.yannis@gmail.com>
2018-12-20 11:26:17 +0100

    Ajout bouton retour menu depuis final screen

---
+++ Assets/RetourMenu.cs
@@ -0,0 +1,13 @@
+﻿using Prototype.NetworkLobby;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetourMenu : MonoBehaviour {
+
+	public void RetourAuMenu()
+    {
+        LobbyManager.s_Singleton.backDelegate.Invoke();
+    }
+
+}
